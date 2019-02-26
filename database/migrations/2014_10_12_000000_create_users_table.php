6deb5541deb8267de47c870bd9295ded45a4f156
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-02-26 19:37:46 +0100

    user model&migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('role_id');
             $table->timestamps();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade'); // povezuje dve tabele po stranom kljucu, i na brisanje role brise i usera sa tom rolom
         });
     }
 
