fc3fdd64b19d0cf5d03788db80ecb7dd515c4afc
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-03-06 00:43:50 +0100

    watch model and migration

---
+++ database/migrations/2019_03_05_205533_create_watches_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('src');
+            $table->string('alt');
+            $table->string('description');
+            $table->string('price');
+            $table->unsignedInteger('brand_id');
+            $table->timestamps();
+
+            $table->foreign('brand_id')
+                ->references('id')
+                ->on('brands')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watches');
+    }
+}
