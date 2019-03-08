215ba6bef3e0948d5d335c235eb103c4d57b85e8
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-03-08 16:02:31 +0100

    eloquent relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,5 +7,38 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     //
+    public function role(){
+
+        return $this->belongsTo(Role::class);
+    }
+
+    public function insert_user(){
+
+        $role_id = request('role_id');
+
+        $firstName = request('firstName');
+
+        $lastName = request('lastName');
+
+        $email = request('email');
+
+        $password = request('password');
+
+        $newUser = new User;
+
+
+        $newUser->first_name = $firstName;
+
+        $newUser->last_name = $lastName;
+
+        $newUser->email = $email;
+
+        $newUser->password = $password;
+
+        $newUser->role_id = $role_id;
+
+        $newUser->save();
+
+    }
     
 }
