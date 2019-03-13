d449db8af0343ff6bc7e5d051a56708546c232a5
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-03-13 04:03:27 +0100

    poll with comments

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -22,55 +22,63 @@ class PollsController extends Controller
         //answer id from data in ajax
         $id_answer = request('id');
 
-        $newAnswer =PollAnswer::find($id_answer);
+        $newAnswer =PollAnswer::with('question')->find($id_answer);
 
-        $poll_id = $newAnswer->poll_id;
+        //if someone tries to change value in inspect element, stop it because you can vote only for active question
+        if ($newAnswer->question->active == 1){
 
-        //pivot table which is used to see if user has voted and for which question
-        $voting = UserVote::where([
-            'user_id'=>$user_id,
-            'poll_id'=>$poll_id
-        ])->first();
+            $poll_id = $newAnswer->poll_id;
 
-        if(!$voting){
+            //pivot table which is used to see if user has voted and for which question
+            $voting = UserVote::where([
+                'user_id'=>$user_id,
+                'poll_id'=>$poll_id
+            ])->first();
 
-        $newAnswer->sum = $newAnswer->sum + 1;
-        
-        //update table poll_answers
-        $newAnswer->save();
+            if(!$voting){
 
-        $user_vote = new UserVote;
-
-        $user_vote->user_id = $user_id;
-
-        $user_vote->poll_id = $poll_id;
-        
-        //insert in pivot table
-        $user_vote->save();
-
-        $answer = PollAnswer::all();
-
-        return $data = [
-            'message' => "Thanks for voting!",
-            'answers' => $answer
-        ];
-
-
-        }
-        else
-        {
-            $answer = PollAnswer::all();
+            $newAnswer->sum = $newAnswer->sum + 1;
             
+            //update table poll_answers
+            $newAnswer->save();
+
+            $user_vote = new UserVote;
+
+            $user_vote->user_id = $user_id;
+
+            $user_vote->poll_id = $poll_id;
+            
+            //insert in pivot table
+            $user_vote->save();
+
+            $answer = PollAnswer::all();
+
             return $data = [
-                'message' => "You already voted, but you can see poll results",
+                'message' => "Thanks for voting!",
                 'answers' => $answer
             ];
 
-            
-        
-        }
 
-     
+            }
+            else
+            {
+                $answer = PollAnswer::all();
+
+                return $data = [
+                    'message' => "You already voted, but you can see poll results",
+                    'answers' => $answer
+                ];
+
+                
+            
+            }
+        }
+        
+
+        else {
+            //aborting if user manipulates answer id
+            abort(403);
+        }
 
         
    }
