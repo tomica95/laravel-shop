e9deef85db8a2adc3680e234b2cfdd2b2863ab21
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-03-14 03:02:03 +0100

    sort 1/3 on shop

--- resources/views/partials/sort.blade.php
+++ resources/views/partials/sort.blade.php
@@ -8,31 +8,23 @@
                         <!-- ##### Single Widget ##### -->
                         <div class="widget catagory mb-50">
                             <!-- Widget Title -->
-                            <h6 class="widget-title mb-30">Catagories</h6>
+                            <h6 class="widget-title mb-30">Brands</h6>
 
-                            <!--  Catagories  -->
+                            <!--  Categories  -->
                             <div class="catagories-menu">
                                 <ul id="menu-content2" class="menu-content collapse show">
                                     <!-- Single Item -->
                                     <li data-toggle="collapse" data-target="#clothing">
-                                        <a href="#">clothing</a>
+                                        <a href="#">Brands</a>
                                         <ul class="sub-menu collapse show" id="clothing">
-                                            <li><a href="#">All</a></li>
-                                            <li><a href="#">Bodysuits</a></li>
-                                            <li><a href="#">Dresses</a></li>
-                                            <li><a href="#">Hoodies &amp; Sweats</a></li>
-                                            <li><a href="#">Jackets &amp; Coats</a></li>
-                                            <li><a href="#">Jeans</a></li>
-                                            <li><a href="#">Pants &amp; Leggings</a></li>
-                                            <li><a href="#">Rompers &amp; Jumpsuits</a></li>
-                                            <li><a href="#">Shirts &amp; Blouses</a></li>
-                                            <li><a href="#">Shirts</a></li>
-                                            <li><a href="#">Sweaters &amp; Knits</a></li>
+                                            @foreach($brands as $brand)
+                                            <li><a href="{{url('shop/'.$brand->id)}}">{{$brand->name}}</a></li>
+                                            @endforeach
                                         </ul>
                                     </li>
                                     <!-- Single Item -->
                                     <li data-toggle="collapse" data-target="#shoes" class="collapsed">
-                                        <a href="#">shoes</a>
+                                        <a href="#">Prices</a>
                                         <ul class="sub-menu collapse" id="shoes">
                                             <li><a href="#">All</a></li>
                                             <li><a href="#">Bodysuits</a></li>
