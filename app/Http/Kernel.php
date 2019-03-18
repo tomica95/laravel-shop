3b06c10731d3f76b40d1ccaad4e05e811402c3ad
tomica95 <45568534+tomica95@users.noreply.github.com>
2019-03-18 14:02:46 +0100

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'notuser'=>\App\Http\Middleware\NotuserMiddleware::class,
 
         'loggeduser' => \App\Http\Middleware\LoggedUserMiddleware::class,
 
--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,11 @@ Route::get('/', 'HomeController@index');
 
 Route::get('/welcome','AuthController@show')->middleware('loggeduser');
 
-Route::post('/register','AuthController@register');
+Route::post('/register','AuthController@register')->middleware('loggeduser');
 
-Route::post('/login','AuthController@login');
+Route::post('/login','AuthController@login')->middleware('loggeduser');
 
-Route::get('/logout','AuthController@logout');
+Route::get('/logout','AuthController@logout')->middleware('notuser');
 
 Route::get('/shop/{id?}','ProductsController@products');
 
@@ -39,47 +39,47 @@ Route::prefix('admin')->group(function(){
         
         Route::get('/','AdminController@index');
 
-        Route::delete('/delete-watch','WatchController@delete_product');
+        Route::delete('/delete-watch','WatchController@delete_product')->middleware('admin');
 
-        Route::delete('/delete-user','UserController@delete_user');
+        Route::delete('/delete-user','UserController@delete_user')->middleware('admin');
 
-        Route::delete('/delete-category','WatchController@delete_category');
+        Route::delete('/delete-category','WatchController@delete_category')->middleware('admin');
 
-        Route::post('/insert-product','WatchController@insert_product');
+        Route::post('/insert-product','WatchController@insert_product')->middleware('admin');
 
-        Route::post('/insert-user','UserController@insert_user');
+        Route::post('/insert-user','UserController@insert_user')->middleware('admin');
 
-        Route::post('/insert-category','WatchController@insert_category');
+        Route::post('/insert-category','WatchController@insert_category')->middleware('admin');
 
-        Route::post('/show-product-update','WatchController@show');
+        Route::post('/show-product-update','WatchController@show')->middleware('admin');
 
-        Route::post('/update-product','WatchController@update_watch');
+        Route::post('/update-product','WatchController@update_watch')->middleware('admin');
 
-        Route::post('/show-user-update','UserController@show_user_update');
+        Route::post('/show-user-update','UserController@show_user_update')->middleware('admin');
 
-        Route::post('/update-user','UserController@update_user');
+        Route::post('/update-user','UserController@update_user')->middleware('admin');
 
-        Route::post('/show-category-update','CategoryController@show_category');
+        Route::post('/show-category-update','CategoryController@show_category')->middleware('admin');
 
-        Route::post('/update-category','CategoryController@update_category');
+        Route::post('/update-category','CategoryController@update_category')->middleware('admin');
 
-        Route::delete('/delete-poll','PollController@delete_poll');
+        Route::delete('/delete-poll','PollController@delete_poll')->middleware('admin');
 
-        Route::post('/insert-poll','PollController@insert_poll');
+        Route::post('/insert-poll','PollController@insert_poll')->middleware('admin');
 
-        Route::post('/poll-for-update','PollController@poll_for_update');
+        Route::post('/poll-for-update','PollController@poll_for_update')->middleware('admin');
 
-        Route::post('/update-poll','PollController@update_poll');
+        Route::post('/update-poll','PollController@update_poll')->middleware('admin');
 
-        Route::post('/insert-answer','PollController@insert_answer');
+        Route::post('/insert-answer','PollController@insert_answer')->middleware('admin');
 
-        Route::delete('/delete-answer','PollController@delete_answer');
+        Route::delete('/delete-answer','PollController@delete_answer')->middleware('admin');
 
-        Route::post('/answer-for-update','PollController@answer_for_update');
+        Route::post('/answer-for-update','PollController@answer_for_update')->middleware('admin');
 
-        Route::post('/update-answer','PollController@update_answer');
+        Route::post('/update-answer','PollController@update_answer')->middleware('admin');
 
-        Route::get('/sort-desc','AdminController@sort_desc');
+        Route::get('/sort-desc','AdminController@sort_desc')->middleware('admin');
         
         
     });
